605237c8e0e77a30e01f7265b08d957ed141b55b
Ryan Zhang <ryanzyy@gmail.com>
2015-05-17 01:07:56 +1000

    add csv_from_google

--- csv_downloader.py
+++ csv_downloader.py
@@ -1,13 +1,13 @@
 from datetime import datetime
 import urllib2
 
-symbol = 'PEP'
-day = '1'
-url = 'http://www.google.com/finance/getprices?i=60&p=' + day + 'd&f=d,o,h,l,c,v&df=cpct&q=' + symbol
-res = urllib2.urlopen(url)
-content = res.read()
+def csv_from_google(symbol, day=10):
+    day = str(day)
+    url = 'http://www.google.com/finance/getprices?i=60&p=' + day + 'd&f=d,o,h,l,c,v&df=cpct&q=' + symbol
+    res = urllib2.urlopen(url)
+    content = res.read()
 
-filename = datetime.now().strftime('%y-%m-%d-') + symbol + '-' + day
+    filename = symbol + datetime.now().strftime('-%y-%m-%d') + '-' + day
 
-with open(filename, 'w') as f:
-    f.write(content)
+    with open(filename, 'w') as f:
+        f.write(content)
