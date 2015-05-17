0cf0c897d66179bb1cdc92490276bce759569f8c
Ryan Zhang <ryanzyy@gmail.com>
2015-05-17 15:45:33 +1000

    add temp google csv datasource

---
+++ csv_file_datasource.py
@@ -0,0 +1,46 @@
+import os
+import re
+
+import numpy as np
+import pandas as pd
+
+from zipline.gens.utils import hash_args
+from zipline.sources.data_source import DataSource
+
+from google_minute import load_raw_dataframe
+
+class TempCSVDataSource(DataSource):
+    def __init__(self, **kwargs):
+        self.stocks = kwargs.get('stocks')
+        self.start = kwargs.get('start')
+        self.end = kwargs.get('end')
+        self._raw_data = None
+
+    @property
+    def mapping(self):
+        return {
+            'dt': (lambda x: x, 'dt'),
+            'sid': (lambda x: x, 'sid'),
+            'price': (float, 'price'),
+            'volume': (int, 'volume'),
+        }
+
+    def raw_data_gen(self):
+        syms = ['KO', 'PEP']
+        for sym in syms:
+            file_name = filter(lambda x: re.compile(sym), os.listdir('.'))
+            df = load_raw_dataframe(file_name)
+            for index, row in df.iterrows():
+                event = {
+                    'dt': index,
+                    'sid': sym,
+                    'price': row['CLOSE'],
+                    'volume': row['VOLUME'],
+                    }
+                    yield event
+
+    @property
+    def raw_data(self):
+        if not self._raw_data:
+            self._raw_data = self.raw_data_gen()
+        return self._raw_data
