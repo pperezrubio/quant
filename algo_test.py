380e668021da6746f7ea24d9c1c9122e95b491e9
Ryan Zhang <ryanzyy@gmail.com>
2015-05-19 00:53:36 +1000

    clean up tester

--- algo_test.py
+++ algo_test.py
@@ -1,33 +1,38 @@
-from zipline.api import order_target, record, symbol, history, add_history
 from zipline.algorithm import TradingAlgorithm
-from zipline.sources.simulated import RandomWalkSource
+from zipline.utils.factory import create_simulation_parameters
 
 from datetime import datetime
 import pytz
 
-import matplotlib.pyplot as plt
 import numpy as np
 
 from csv_file_datasource import TempCSVDataSource
-
 
 def initialize(context):
     pass
 
 def handle_data(context, data):
-    order_target('KO', 100)
-    order_target('PEP', 100)
+    context.order_target('KO', 1)
+    context.order_target('PEP', 1)
 
-    record(
-        ko=data[symbol('KO')].price,
+    context.record(
+        ko=data['KO'].price,
         pep=data['PEP'].price
         )
 
 if __name__ == '__main__':
-    start = datetime(2015,5,7,0,0,0,0, pytz.utc)
-    end = datetime(2015,5,16,0,0,0,0, pytz.utc)
+    start = datetime(2015, 5, 14, 3, 31, 0, 0, pytz.utc)
+    end = datetime(2015, 5, 15, 9, 59, 0, 0, pytz.utc)
+
     data = TempCSVDataSource(stocks=['KO', 'PEP'], start=start, end=end)
-    algo = TradingAlgorithm(handle_data=handle_data, initialize=initialize)
+
+    sim_params = create_simulation_parameters(
+        start=start, end=end, capital_base=5000, data_frequency='minute')
+
+    algo = TradingAlgorithm(
+            handle_data=handle_data,
+            initialize=initialize,
+            sim_params=sim_params
+            )
     results = algo.run(data)
-    from IPython import embed
-    embed()
+    print results # only daily_stats is supported by default Algo class
